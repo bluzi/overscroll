d08887012e14a3bf896d359e472753dc11213c3e
Eliran Pe'er <eliran.peer@forter.com>
2017-09-23 20:03:42 +0300

    Ensure getBattery support

--- overscroll.js
+++ overscroll.js
@@ -1,34 +1,41 @@
-window.overscroll = function(callback) {
-    if (navigator.platform !== 'MacIntel'
-        || (battery.charging && battery.chargingTime === 0)) {
-            // Not a MacBook
+window.overscroll = function (callback) {
+    if (!navigator || !navigator.getBattery) {
+        // getBattery is not supported on this browser
         return;
     }
 
-    if (!window.overscroll.callbacks) {
-        window.overscroll.callbacks = [callback];
+    navigator.getBattery().then(function(battery) {
+        if (navigator.platform !== 'MacIntel'
+            || (battery.charging && battery.chargingTime === 0)) {
+            // Not a MacBook
+            return;
+        }
 
-        var mostReventScrollEvent = new Date().getTime();        
+        if (!window.overscroll.callbacks) {
+            window.overscroll.callbacks = [callback];
 
-        window.addEventListener('wheel', function(e) {
-            var timeOfEvent = new Date().getTime();
-            var direction = e.deltaY < 0 ? 'up' : 'down';
+            var mostReventScrollEvent = new Date().getTime();
 
-            setTimeout(function() {
-                if ((timeOfEvent - 1000) > mostReventScrollEvent 
+            window.addEventListener('wheel', function (e) {
+                var timeOfEvent = new Date().getTime();
+                var direction = e.deltaY < 0 ? 'up' : 'down';
+
+                setTimeout(function () {
+                    if ((timeOfEvent - 1000) > mostReventScrollEvent
                         && (document.documentElement.scrollTop === 0 && direction === 'up')
                         || ((window.innerHeight + window.scrollY) >= document.body.scrollHeight && direction === 'down')) {
-                    for(var key in window.overscroll.callbacks) {
-                        window.overscroll.callbacks[key]({
-                            direction: direction
-                        });
+                        for (var key in window.overscroll.callbacks) {
+                            window.overscroll.callbacks[key]({
+                                direction: direction
+                            });
+                        }
                     }
-                }
+                });
             });
-        });
 
-        window.addEventListener('scroll', function(e) { mostReventScrollEvent = new Date(); });
-    } else { 
-        window.overscroll.callbacks.push(callback);
-    }
+            window.addEventListener('scroll', function (e) { mostReventScrollEvent = new Date(); });
+        } else {
+            window.overscroll.callbacks.push(callback);
+        }
+    });
 }
